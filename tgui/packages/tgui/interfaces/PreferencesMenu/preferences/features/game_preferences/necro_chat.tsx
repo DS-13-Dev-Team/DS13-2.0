28c9f81b6c692d2fa02317966fdd0a51ba970fd4
Sym <gamer2418@hotmail.com>
2024-02-03 15:09:20 -0500

    Fixes necro chat preferences
    Fixes how necromorph chat works with ghosts, and gives necro chat a proper preference instead of a legacy one.

---
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/game_preferences/necro_chat.tsx
@@ -0,0 +1,8 @@
+import { CheckboxInput, FeatureToggle } from "../base";
+
+export const necro_chat: FeatureToggle = {
+  name: "Necromorph Chat",
+  category: "CHAT",
+  description: "Do you want to hear necromorph chat as a ghost?",
+  component: CheckboxInput,
+};
